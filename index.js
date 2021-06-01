3f061bab835cb00eb1504b2f43364af631d4f053
Sibel <sbaslamisli@yahoo.com>
2021-05-31 20:55:24 -0400

    responsive navbar

--- index.js
+++ index.js
@@ -5,4 +5,15 @@ let navBarToggle = document.getElementById('js-navbar-toggle');
 
 navBarToggle.addEventListener('click', function() {
     mainNav.classList.toggle('active');
+
+    const icon = this.querySelector('i');
+
+    if (icon.classList.contains('fa-bars')) {
+        icon.classList.remove('fa-bars');
+        icon.classList.add('fa-times');
+    } else {
+        icon.classList.remove('fa-times');
+        icon.classList.add('fa-bars');
+    }
+
 });
