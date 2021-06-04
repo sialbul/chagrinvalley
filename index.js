1d4c5e999a4b0097fa9bc8f918fd568fe4047124
Sibel <sbaslamisli@yahoo.com>
2021-06-04 00:19:00 -0400

    still working on moreaboutus section

--- index.js
+++ index.js
@@ -20,3 +20,41 @@ navBarToggle.addEventListener('click', function() {
 
 /*animation*/
 AOS.init();
+
+
+
+
+//getting info
+class MoreAboutUs {
+    async getData() {
+        try {
+            let result = await fetch('data.json');
+            let data = await result.json();
+
+            let moreAboutUsAll = data.aboutUsHeaders;
+            moreAboutUsAll = moreAboutUsAll.map(aboutUsHeader => {
+                const { id, img, header, exp } = aboutUsHeader;
+                return { id, img, header, exp }
+            })
+            return moreAboutUsAll
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+
+function changeImage() {
+    const viewAboutUs = `
+    <div class="aboutUsInfoParargaf">
+    <h2>${aboutUsHeader.exp}</h2>
+
+    <p>Cleveries envisions a diverse community in which kids are respected and heard by a collaborative support system of organized and trained adults, who are teachers and friends at the same time and help kids realize their potential.</p>
+    <button class="learnMoreButton2">LEARN MORE</button>
+</div>
+<div class="aboutUsInfoPicture">
+    <img id="aboutUsInfoPhotos" src="./images/schoolage.jpg">
+</div>`
+    document.getElementById("aboutUsInfo").innerText = viewAboutUs;
+
+};
