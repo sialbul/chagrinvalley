c6546580263f05398f39fdbbd13e38ab0c065686
Sibel <sbaslamisli@yahoo.com>
2021-06-09 01:30:46 -0400

    about us section-react

--- chagrin-valley/src/App.js
+++ chagrin-valley/src/App.js
@@ -1,7 +1,32 @@
-import "./App.css";
+import Navbar from "./pages/Navbar";
+import Home from "./pages/Home";
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Redirect,
+} from "react-router-dom";
 
 function App() {
-    return;
+    return (
+        <Router>
+            <Navbar />
+            <Home />
+            {/* <div className="container">
+                <Switch>
+                    <Route exact path="/" component={home} />
+                    <AuthRoute exact path="/login" component={login} />
+                    <AuthRoute exact path="/signup" component={signup} />
+                    <Route exact path="/users/:handle" component={user} />
+                    <Route
+                        exact
+                        path="/users/:handle/scream/:screamId"
+                        component={user}
+                    />
+                </Switch>
+            </div> */}
+        </Router>
+    );
 }
 
 export default App;
