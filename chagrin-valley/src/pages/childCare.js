c78607cdf02042a5f7813c56fe2049168b464471
Sibel <sbaslamisli@yahoo.com>
2021-06-29 01:10:12 -0400

    childcare page table is done

--- chagrin-valley/src/pages/childCare.js
+++ chagrin-valley/src/pages/childCare.js
@@ -1,5 +1,169 @@
 import React from "react";
 
 export default function ChildCare() {
-    return <div>Childcare </div>;
+    return (
+        <div>
+            <div id="pageContainer">
+                <span class="sectionHeader"> Tuition and Fees </span>
+                <table>
+                    <tr>
+                        <th></th>
+                        <th>1st Child</th>
+                        <th>2nd Child</th>
+                        <th>3rd Child</th>
+                        <th>Part Time</th>
+                        <th>Daily</th>
+                        <th>12.5hr a week</th>
+                    </tr>
+                    <tr>
+                        <td>
+                            INFANTS
+                            <br />6 weeks- 21months
+                        </td>
+                        <td>232.00</td>
+                        <td>208.80 </td>
+                        <td>185.60</td>
+                        <td>168.00</td>
+                        <td>56.00</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            INFANTS with
+                            <br />
+                            Diaper and Wipes
+                        </td>
+                        <td>250.00</td>
+                        <td>225.00 </td>
+                        <td>200.00</td>
+                        <td>177.00</td>
+                        <td>59.00</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            TODDLER <br />
+                            21 months-3 years{" "}
+                        </td>
+                        <td>205.00</td>
+                        <td>184.50 </td>
+                        <td>164.00</td>
+                        <td>153.00</td>
+                        <td>51.00</td>
+                        <td>85.00**</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            TODDLER <br />
+                            Pullups and Wipes{" "}
+                        </td>
+                        <td>223.00</td>
+                        <td>200.70 </td>
+                        <td>178.40</td>
+                        <td>162.00</td>
+                        <td>54.00</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            PRESCHOOL <br />3 years-5 years{" "}
+                        </td>
+                        <td>187.00</td>
+                        <td>168.30 </td>
+                        <td>149.60</td>
+                        <td>135.00</td>
+                        <td>45.00</td>
+                        <td>73.00**</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            SCHOOL AGE <br />
+                        </td>
+                        <td>177.00</td>
+                        <td>159.30 </td>
+                        <td>141.60</td>
+                        <td>126.00</td>
+                        <td>42.00</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Before and After</td>
+                        <td>93.00</td>
+                        <td>83.70 </td>
+                        <td>74.40</td>
+                        <td>83.70</td>
+                        <td>28.00</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Before </td>
+                        <td>48.00</td>
+                        <td>43.20 </td>
+                        <td>38.40</td>
+                        <td>43.20</td>
+                        <td>15.00</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>After </td>
+                        <td>75.00</td>
+                        <td>67.50 </td>
+                        <td>60.00</td>
+                        <td>67.50</td>
+                        <td>23.00</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            CHAGRIN FALLS
+                            <br /> Before OR After
+                        </td>
+                        <td>93.00</td>
+                        <td>83.70 </td>
+                        <td>74.40</td>
+                        <td>83.70</td>
+                        <td>28.00</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            CHAGRIN FALLS
+                            <br /> Before AND After
+                        </td>
+                        <td>118.00</td>
+                        <td>106.20 </td>
+                        <td>94.40</td>
+                        <td>99.00</td>
+                        <td>33.00</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Transportation to <br /> surrounding schools
+                        </td>
+                        <td>
+                            25.00
+                            <br />+ weekly
+                        </td>
+                        <td> </td>
+                        <td></td>
+                        <td></td>
+                        <td>
+                            10.00
+                            <br />+ daily
+                        </td>
+                        <td></td>
+                    </tr>
+                </table>
+                <h2>RATE WILL VERY BASED ON TRANSPORTATION NEEDS</h2>
+                <p>
+                    <span id="parHeader"> Registration Fee: </span>
+                    $50.00 per family *Due upon registration & any lengthy
+                    withdraw and re-enroll *non-refundable <br />
+                    <span id="parHeader"> Preschool: </span> 3 years old and
+                    Potty trained.
+                </p>
+            </div>
+        </div>
+    );
 }
