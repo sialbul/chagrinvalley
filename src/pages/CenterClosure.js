937951fdfa8f543364c920b8fd3c31d16b98145a
sialbul <sbaslamisli@yahoo.com>
2021-09-13 23:04:39 -0400

    styling

--- src/pages/CenterClosure.js
+++ src/pages/CenterClosure.js
@@ -10,54 +10,52 @@ import Flag from "../assets/photo-gallery/usa-flag.png";
 import { NavLink } from "react-router-dom";
 
 export default function CenterClosure() {
-    return (
-        <div id="galleryContainer">
-            <p className="sectionHeader">Center Closure</p>
-            <br />
-            <br />
-            <h2> The Center will be closed for the following Holidays:</h2>
-            <br />
-            <div id="centerClosureList">
-                <img src={Newyear} className="icon" />
-                New Year’s Day
-                <br />
-                <img src={Flag} className="icon" />
-                Memorial Day
-                <br />
-                <img src={July} className="icon" />
-                4th of July
-                <br />
-                <img src={Labor} className="icon" />
-                Labor Day
-                <br />
-                <img src={Thanks} className="icon" />
-                Thanksgiving Day
-                <br />
-                <img src={Thanks2} className="icon" />
-                Day after Thanksgiving
-                <br />
-                <img src={Santa} className="icon" />
-                Close early Christmas Eve
-                <br />
-                <img src={Christmas} className="icon" />
-                Christmas Day <br />
-                Inclement weather closures will be announced through the REMIND
-                app and also posted on FOX 8 News.
-            </div>
-            <div>
-                <p className="sectionHeaderPrograms">
-                    <NavLink to="/gallery">
-                        {" "}
-                        Gallery{" "}
-                        <i
-                            className="fa fa-angle-double-right"
-                            aria-hidden="true"></i>
-                        <i
-                            className="fa fa-angle-double-right"
-                            aria-hidden="true"></i>
-                    </NavLink>
-                </p>
-            </div>
+  return (
+    <div>
+      <div id="galleryTop">
+        <p className="sectionHeader">Center Closure</p>
+        <br />
+        <br />
+        <h2> The Center will be closed for the following Holidays:</h2>
+        <br />
+        <div id="centerClosureList">
+          <img src={Newyear} className="icon" />
+          New Year’s Day
+          <br />
+          <img src={Flag} className="icon" />
+          Memorial Day
+          <br />
+          <img src={July} className="icon" />
+          4th of July
+          <br />
+          <img src={Labor} className="icon" />
+          Labor Day
+          <br />
+          <img src={Thanks} className="icon" />
+          Thanksgiving Day
+          <br />
+          <img src={Thanks2} className="icon" />
+          Day after Thanksgiving
+          <br />
+          <img src={Santa} className="icon" />
+          Close early Christmas Eve
+          <br />
+          <img src={Christmas} className="icon" />
+          Christmas Day <br />
+          Inclement weather closures will be announced through the REMIND app
+          and also posted on FOX 8 News.
         </div>
-    );
+      </div>
+      <div>
+        <p className="sectionHeaderPrograms">
+          <NavLink to="/gallery">
+            {" "}
+            Gallery{" "}
+            <i className="fa fa-angle-double-right" aria-hidden="true"></i>
+            <i className="fa fa-angle-double-right" aria-hidden="true"></i>
+          </NavLink>
+        </p>
+      </div>
+    </div>
+  );
 }
