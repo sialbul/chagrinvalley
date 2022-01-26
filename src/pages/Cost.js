2db035531add5383658bad7068b690ac50bff111
sialbul <sbaslamisli@yahoo.com>
2022-01-25 20:24:18 -0500

    update the fees

--- src/pages/Cost.js
+++ src/pages/Cost.js
@@ -22,9 +22,9 @@ export default function Cost() {
                                 INFANTS
                                 <br />6 weeks- 21months
                             </td>
-                            <td>232.00</td>
-                            <td>208.80 </td>
-                            <td>185.60</td>
+                            <td>269.00</td>
+                            <td>242.80 </td>
+                            <td>198.00</td>
                             <td></td>
                         </tr>
                         <tr>
@@ -33,9 +33,9 @@ export default function Cost() {
                                 <br />
                                 Diaper and Wipes
                             </td>
-                            <td>250.00</td>
-                            <td>225.00 </td>
-                            <td>200.00</td>
+                            <td>290.00</td>
+                            <td>261.00 </td>
+                            <td>2220.00</td>
 
                             <td></td>
                         </tr>
@@ -44,20 +44,20 @@ export default function Cost() {
                                 TODDLER <br />
                                 21 months-3 years{" "}
                             </td>
-                            <td>205.00</td>
-                            <td>184.50 </td>
-                            <td>164.00</td>
+                            <td>239.00</td>
+                            <td>215.00 </td>
+                            <td>185.00</td>
 
-                            <td>90.00**</td>
+                            <td>98.00**</td>
                         </tr>
                         <tr>
                             <td>
                                 TODDLER <br />
                                 Pullups and Wipes{" "}
                             </td>
-                            <td>223.00</td>
-                            <td>200.70 </td>
-                            <td>178.40</td>
+                            <td>260.00</td>
+                            <td>234.00 </td>
+                            <td>198.00</td>
 
                             <td></td>
                         </tr>
@@ -65,41 +65,41 @@ export default function Cost() {
                             <td>
                                 PRESCHOOL <br />3 years-5 years{" "}
                             </td>
-                            <td>187.00</td>
-                            <td>168.30 </td>
-                            <td>149.60</td>
+                            <td>209.00</td>
+                            <td>188.00 </td>
+                            <td>159.00</td>
 
-                            <td>78.00**</td>
+                            <td>88.00**</td>
                         </tr>
                         <tr>
                             <td>
                                 SCHOOL AGE <br />
                             </td>
-                            <td>177.00</td>
-                            <td>159.30 </td>
-                            <td>141.60</td>
+                            <td>189.00</td>
+                            <td>170.00 </td>
+                            <td>151.00</td>
 
                             <td></td>
                         </tr>
                         <tr>
                             <td>Before and After</td>
-                            <td>93.00</td>
-                            <td>83.70 </td>
-                            <td>83.70</td>
+                            <td>96.00</td>
+                            <td>86.40 </td>
+                            <td>85.00</td>
                             <td></td>
                         </tr>
                         <tr>
                             <td>Before </td>
-                            <td>48.00</td>
-                            <td>43.20 </td>
-                            <td>43.20</td>
+                            <td>49.00</td>
+                            <td>44.10 </td>
+                            <td>44.00</td>
                             <td></td>
                         </tr>
                         <tr>
                             <td>After </td>
-                            <td>75.00</td>
-                            <td>67.50 </td>
-                            <td>67.50</td>
+                            <td>77.00</td>
+                            <td>69.30 </td>
+                            <td>69.00</td>
                             <td></td>
                         </tr>
                     </tbody>
@@ -108,7 +108,7 @@ export default function Cost() {
                 <h2>RATE WILL VERY BASED ON TRANSPORTATION NEEDS</h2>
                 <p id="rateExplanation">
                     <span id="parHeader"> Registration Fee: </span>
-                    $50.00 per family *Due upon registration & any lengthy
+                    $75.00 per family *Due upon registration & any lengthy
                     withdraw and re-enroll *non-refundable <br />
                     <span id="parHeader"> Preschool: </span> 3 years old and
                     Potty trained.
@@ -121,27 +121,24 @@ export default function Cost() {
                     receive 5 or less hours of child care per day or are
                     enrolled 2-3 days a week
                     <br />
-                    <span id="parHeader"> Daily Rate: </span>minimum of 2 full
-                    days
-                    <br />
                     <span id="parHeader"> Snow Days:</span> As a courtesy to our
                     families, you may drop off your school aged child (up to the
                     age of 12) and be charged the daily rate. This service has
                     limited enrollment. PLEASE ALWAYS CALL AHEAD FOR
                     CONFIRMATION. For Families already enrolled in our Before &
-                    After care Program the fee is $21.00 a day.
+                    After care Program the fee is $29.00 a day.
                     <br />
-                    <span id="parHeader"> Late Pick Up Fee: </span>$5 per child
-                    every minute here past 5:30 pm Monday--Thursday & 5:00pm
+                    <span id="parHeader"> Late Pick Up Fee: </span>$15 per child
+                    every 15 minuteS here past 5:30 pm Monday--Thursday & 5:00pm
                     Friday.
                     <br />
                     <span id="parHeader"> **2 ½ Hour Preschool: </span>
-                    Monday thru Friday 9-11:30 am. Toddler $90 Preschool $78
+                    Monday thru Friday 9-11:30 am. Toddler $98 Preschool $88
                 </p>
                 <br />
                 <h2>
                     ***Hours of Operation: Monday thru Thursday 6am—5:30pm
-                    Friday 6am—5pm***effective 08/02/21
+                    Friday 6am—5pm***effective 01/24/2022S
                 </h2>
                 <br />
             </div>
